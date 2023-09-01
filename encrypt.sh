35c43e2ee1f3766b1c38e46a5520cf8006b0eb69
Ubuntu <ubuntu@ip-172-31-36-228.ap-southeast-1.compute.internal>
2023-09-01 13:00:39 +0000

    Update

--- encrypt.sh
+++ encrypt.sh
@@ -1,3 +1,5 @@
 mkdir -p config/kibana
 docker compose -f kibana.docker-compose.yml run --rm kibana bin/kibana-encryption-keys generate > config/kibana/encrypt.txt
 python3 parse.py
+chmod 777 config/kibana/encrypt.sh
+docker compose -f kibana.docker-compose.yml run --rm kibana sh temp/encrypt.sh
--- parse.py
+++ parse.py
@@ -23,8 +23,6 @@ with open('./config/kibana/encrypt.txt', 'r') as file:
 with open('./config/kibana/encrypt.json', 'w') as file:
     json.dump(json_output, file)
 
-os.remove('./config/kibana/encrypt.txt')
-
 command_text = [
     cmd_("Run Kibana container first"),
     cmd_(DOCKER_CMD, 'up -d'),
@@ -34,29 +32,32 @@ command_text = [
     '\n',
     cmd_("Pass these into kibana terminal one by one"),
     '\n',
-    cmd_(TARGET_BIN, 'create'),
 ]
 
+container_commands = [
+    cmd_(TARGET_BIN, 'create'),
+    *[cmd_(
+        'echo', f'"{json_output[key]}"', '|', TARGET_BIN, 'add', '-f', key, ) for key in json_output
+    ],
+    cmd_('cp config/kibana.keystore temp')
+]
 
-for key in json_output:
-    command_text.append(cmd_(
-            'echo', f'"{json_output[key]}"',
-            '|', 
-            TARGET_BIN, 'add', '-f', key, 
-        )
-    )
+container_text = ''.join(container_commands)
+with open('./config/kibana/encrypt.sh', 'w') as file:
+    file.writelines(container_text)
 
-command_text.append(cmd_('cp config/kibana.keystore temp'))
+command_text.append(container_text)
+
 command_text.append(cmd_('exit'))
 command_text.append('\n')
 command_text.append(cmd_("Stop Kibana container"))
 command_text.append(cmd_(DOCKER_CMD, 'down'))
 command_text.append('\n')
-command_text.append(cmd_("Remove encrypt.json"))
-command_text.append(cmd_("rm config/kibana/encrypt.json -f"))
+command_text.append(cmd_("Remove encrypt.json and encrypt.txt"))
+command_text.append(cmd_("rm config/kibana/encrypt.* -f"))
 command_text.append('\n')
 command_text.append(cmd_("Remove this file"))
 command_text.append(cmd_("rm keystore-command.txt -f"))
     
-with open('./keystore-command.txt', 'w') as file:
-    file.writelines(command_text)
+# with open('./keystore-command.txt', 'w') as file:
+#     file.writelines(command_text)
